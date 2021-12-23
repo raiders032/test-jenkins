a8c800c99550a18ec39b751e4ce1fcf0296cc788
raiders032 <nameks@naver.com>
2021-12-23 16:42:53 +0900

    init

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM jenkins/jenkins:2.303.3-jdk11
+
+# 설치 위자드 비활성화
+ENV JAVA_OPTS -Djenkins.install.runSetupWizard=false
+
+# JCasC가 읽을 설정 파일 위치 지정하는 환경 변수
+ENV CASC_JENKINS_CONFIG /var/jenkins_home/casc.yml
+
+# Jenkins Docker image에 Docker 설치
+USER root
+RUN curl -sSL https://get.docker.com/ | sh
+RUN usermod -a -G docker jenkins
+USER jenkins
+
+# plugins.txt에 설치하고자 하는 플러그를 명시하고 install-plugins.sh을 통해 설치
+COPY plugins.txt /usr/share/jenkins/ref/plugins.txt
+RUN /usr/local/bin/install-plugins.sh < /usr/share/jenkins/ref/plugins.txt
+
+# 앞에 작성한 설정 파일 casc.yaml 복사
+COPY casc.yml /var/jenkins_home/casc.yml
+
+# 젠킨스 기본 잡 파일 복사
+COPY seedJob.xml /usr/share/jenkins/ref/jobs/seed-job/config.xml
---
+++ createJobs.groovy
@@ -0,0 +1,30 @@
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/raiders032/spring-boot-api-example.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
+
+pipelineJob('theme-park-job-docker') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/raiders032/spring-boot-api-example.git'
+                    }
+                    branch 'master'
+                    scriptPath('Jenkinsfile-docker')
+                }
+            }
+        }
+    }
+}
